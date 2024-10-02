55446cc6bcb0f85781d541116099154f2136c5e7
Brandon Hamilton <bham2015@gmail.com>
2024-10-02 12:06:47 -0500

    Set up project with the included CSS and React fragment.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div id="container">
+        <h1>Hello React Router!</h1>
+        <div id="navbar">{/* navigation here */}</div>
+        <div id="main-section">{/* routes here */}</div>
+      </div>
+    </>
+  );
 }
 
 export default App;
